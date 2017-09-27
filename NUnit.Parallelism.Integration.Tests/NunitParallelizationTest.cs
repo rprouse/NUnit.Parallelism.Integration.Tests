7381656c88115d84cabf56ba7fd28a9318220538
Rob Prouse <rob.prouse@bentley.com>
2017-09-27 05:31:50 -0400

    Project to test nunit/nunit#2445 and related issues

---
+++ NUnit.Parallelism.Integration.Tests/NunitParallelizationTest.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+
+namespace NUnitParallelismIntegrationTests
+{
+    [TestFixture]
+    public class NunitParallelizationTest
+    {
+        [Parallelizable(ParallelScope.All)]
+        [Test]
+        public void Test([Range(1, 10)] int x)
+        {
+            TestContext.WriteLine($"Test {x}");
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            TestContext.WriteLine("OneTimeSetup");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            TestContext.WriteLine("OneTimeTearDown");
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
+    public class NunitParallelizationTest2
+    {
+        [Test]
+        public void Test([Range(1, 10)] int x)
+        {
+            TestContext.WriteLine($"Test {x}");
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            TestContext.WriteLine("OneTimeSetup");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            TestContext.WriteLine("OneTimeTearDown");
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.Children)]
+    public class NunitParallelizationTest3
+    {
+        [Test]
+        public void Test([Range(1, 10)] int x)
+        {
+            TestContext.WriteLine($"Test {x}");
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            TestContext.WriteLine("OneTimeSetup");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            TestContext.WriteLine("OneTimeTearDown");
+        }
+    }
+
+    [TestFixture]
+    [RequiresThread(System.Threading.ApartmentState.STA)]
+    public class NunitParallelizationTestSTA
+    {
+        [Test]
+        [Parallelizable(ParallelScope.All)]
+        public void Test([Range(1, 10)] int x)
+        {
+            TestContext.WriteLine($"Test {x}");
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            TestContext.WriteLine("OneTimeSetup");
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            TestContext.WriteLine("OneTimeTearDown");
+        }
+    }
+}

4dc5042f1a099e14d23ac58d315496005ca675be
Rob Prouse <rob.prouse@bentley.com>
2017-09-27 05:57:32 -0400

    Verifying nunit/nunit#2454

---
+++ NUnit.Parallelism.Integration.Tests/Issue2454.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+[assembly: Parallelizable(ParallelScope.Fixtures)]
+
+namespace NUnit.Parallelism.Integration.Tests
+{
+
+    [SetUpFixture]
+    public class TestsSetUpFixture
+    {
+
+    }
+
+    [TestFixture]
+    //[Parallelizable(ParallelScope.Self)]
+    public class Issue2454TestFixture1
+    {
+        [Test]
+        public void TestFixture1_Test()
+        {
+            TestContext.Progress.WriteLine($"Starting {nameof(TestFixture1_Test)}");
+            Thread.Sleep(1000);
+            TestContext.Progress.WriteLine($"Finishing {nameof(TestFixture1_Test)}");
+        }
+    }
+
+    [TestFixture]
+    [NonParallelizable]
+    public class Issue2454TestFixture2
+    {
+        [Test]
+        public void TestFixture2_Test()
+        {
+            TestContext.Progress.WriteLine($"Starting {nameof(TestFixture2_Test)}");
+            Thread.Sleep(1000);
+            TestContext.Progress.WriteLine($"Finishing {nameof(TestFixture2_Test)}");
+        }
+    }
+
+    [TestFixture]
+    //[Parallelizable(ParallelScope.Self)]
+    public class Issue2454TestFixture3
+    {
+        [Test]
+        public void TestFixture3_Test()
+        {
+            TestContext.Progress.WriteLine($"Starting {nameof(TestFixture3_Test)}");
+            Thread.Sleep(1000);
+            TestContext.Progress.WriteLine($"Finishing {nameof(TestFixture3_Test)}");
+        }
+    }
+}

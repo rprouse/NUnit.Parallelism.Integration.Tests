ae85dda2d6f350831bd98a09db8967109d32b9ae
Rob Prouse <rob@prouse.org>
2017-10-30 20:57:48 -0400

    tests https://github.com/nunit/nunit/issues/2467

---
+++ tests/SetupFixture/Class1.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace BugNUnit
+{
+    [SetUpFixture]
+    public class AssemblySetup
+    {
+        public AssemblySetup()
+        {
+        }
+    }
+
+    [TestFixture]
+    public class TestInNamespace
+    {
+    }
+}
+
+namespace OtherNamespace
+{
+    [TestFixture]
+    public class TestInAnotherNamespace
+    {
+    }
+}

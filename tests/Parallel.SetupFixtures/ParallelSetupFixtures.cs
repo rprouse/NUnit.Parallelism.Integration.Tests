feba414662e344b2fbe10826a0a5f63f81bab6ce
Rob Prouse <rob@prouse.org>
2017-10-30 20:27:47 -0400

    Test of NUnit3 sometimes hangs if SetUpFixtures are run in parallel #1239

---
+++ tests/Parallel.SetupFixtures/ParallelSetupFixtures.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using NUnit.Framework;
+
+// Tests Issue #1239
+
+[assembly: Timeout(60000)]
+[assembly: Parallelizable(ParallelScope.Children)]
+
+namespace Parallel.SetupFixtures
+{
+    [SetUpFixture]
+    public class FooSetUp
+    {
+        [OneTimeSetUp]
+        public void FooSetUpMethod()
+        {
+            Console.WriteLine("FooSetUp");
+        }
+    }
+
+    // [Parallelizable(ParallelScope.None)]  // if this line is uncommented, then NUnit will not hang
+    [SetUpFixture]
+    public class BarSetUp
+    {
+        [OneTimeSetUp]
+        public void BarSetUpMethod()
+        {
+            Thread.Sleep(500); // if this line is commented out, then NUnit will not hang
+            Console.WriteLine("BarSetUp");
+        }
+    }
+
+    [TestFixture]
+    public class MyTests
+    {
+        [Test]
+        public void MyTest()
+        {
+            Console.WriteLine("MyTest");
+        }
+    }
+}

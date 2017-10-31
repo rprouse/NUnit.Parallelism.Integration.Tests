ca40df4b9cd9629cd8aef66e059f9f5a8fe2925b
Rob Prouse <rob@prouse.org>
2017-10-30 21:09:38 -0400

    Fix for https://github.com/nunit/nunit/issues/2471

---
+++ Parallel.Children/Deserialization.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+
+// Tests https://github.com/nunit/nunit/issues/2471
+
+namespace Parallel.Children
+{
+    [TestFixture]
+    class Deserialization
+    {
+        public static IEnumerable<TimeSpan> ShouldDeserializeAllCases()
+              => Enumerable.Repeat(0, 5).Select(x => TimeSpan.FromSeconds(2));
+
+        [TestCaseSource("ShouldDeserializeAllCases"), Parallelizable(ParallelScope.Children)]
+        public void ShouldDeserializeAll(TimeSpan t)
+        {
+            Thread.Sleep(t);
+            Assert.AreEqual(1, 1);
+        }
+    }
+}

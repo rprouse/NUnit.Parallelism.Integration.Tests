60ae779c948cde9dbeaa69d5fa294e42aa335252
Rob Prouse <rob@prouse.org>
2017-11-05 15:15:41 -0500

    Testing [Parallelizable] hangs after a few tests #2438

---
+++ tests/Parallel.Children/NunitParallelizationTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+// Test for issue #2438 - [Parallelizable] hangs after a few tests
+
+namespace Parallel.Children
+{
+    [TestFixture]
+    public class NunitParallelizationTest
+    {
+        [Parallelizable(ParallelScope.All)]
+        [Test]
+        public void Test([Range(1, 10)] int x)
+        {
+
+        }
+    }
+}

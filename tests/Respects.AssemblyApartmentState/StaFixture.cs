14b1f3d12d08f96fc78c35516ac4bbc6cc8738ef
Rob Prouse <rob@prouse.org>
2017-11-05 15:31:50 -0500

    Tests Regression in 3.8.1: ApartmentAttribute no longer works when applied to an assembly #2426

---
+++ tests/Respects.AssemblyApartmentState/StaFixture.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using NUnit.Framework;
+
+// Tests Regression in 3.8.1: ApartmentAttribute no longer works when applied to an assembly #2426
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace Respects.AssemblyApartmentState
+{
+    [TestFixture]
+    public class StaFixture
+    {
+        [Test]
+        public void Test()
+        {
+            if (Thread.CurrentThread.GetApartmentState() != ApartmentState.STA)
+            {
+                throw new ThreadStateException("The current threads apartment state is not STA");
+            }
+        }
+    }
+}

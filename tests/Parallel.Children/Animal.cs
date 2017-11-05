1d82add3ec59dd179673ddcf7701f4b07f6e3965
Rob Prouse <rob@prouse.org>
2017-11-05 15:12:19 -0500

    Testing issue #2459 - Parallizable(ParallelScope.Children)

---
+++ tests/Parallel.Children/Animal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+
+namespace Parallel.Children
+{
+    public class Animal
+    {
+        public Animal()
+        {
+            Console.Write("Init. Thread #" + Thread.CurrentThread.ManagedThreadId + " ");
+            Thread.Sleep(1000);
+        }
+
+        public void Do()
+        {
+            Console.WriteLine("Done. Thread #" + Thread.CurrentThread.ManagedThreadId + " for " + ToString());
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public Cat()
+        {
+            Console.WriteLine("I'm a cat");
+        }
+
+        public override string ToString()
+        {
+            return "cat";
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("I'm a dog");
+        }
+
+        public override string ToString()
+        {
+            return "dog";
+        }
+    }
+
+    public class Base
+    {
+        public static readonly ThreadLocal<Animal> ThreadLocalContext = new ThreadLocal<Animal>();
+    }
+}
---
+++ tests/Parallel.Children/Children.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+
+namespace Parallel.Children
+{
+    public class BaseTest<T> where T : Animal, new()
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            Base.ThreadLocalContext.Value = new T();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Base.ThreadLocalContext.Value.Do();
+        }
+    }
+
+    [TestFixture]
+    [Parallelizable(ParallelScope.Children)]
+    [TestFixture(typeof(Dog))]
+    [TestFixture(typeof(Cat))]
+    public class Test1<T> : BaseTest<T> where T : Animal, new()
+    {
+        [Test, TestCaseSource(typeof(DataProvider), nameof(DataProvider.TestCases))]
+        public void Test(string url)
+        {
+            Console.WriteLine(url + " " + Base.ThreadLocalContext.Value);
+        }
+    }
+
+    public class DataProvider
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("hello");
+                yield return new TestCaseData("hi");
+                yield return new TestCaseData("bonjour");
+                yield return new TestCaseData("hola");
+                yield return new TestCaseData("hey");
+            }
+        }
+    }
+}
